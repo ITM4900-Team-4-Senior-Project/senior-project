e9cd42c996a12b0a123c95b6b8e796696574b6a0
Xuan Huynh <xqhuynh@gmail.com>
2021-03-09 22:06:14 -0600

    Create Services component

---
+++ src/components/Services/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <service>
+      <div className="header-title">
+        <h3 className="service-subject-header">SERVICES</h3>
+        <hr className="service-hr" />
+        <p className="service-description">
+          Below is a list of our super affordable rates per subject. Book your
+          tutor today.
+        </p>
+      </div>
+
+      <div className="service-row row-cols-1 row-cols-md-3 g-4">
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="images/calculator.jpg"
+              className="card-img-top"
+              alt="calculator-img"
+            />
+            <div className="card-body">
+              <h4 className="card-title">MATH</h4>
+              <p className="card-text">$15/hour</p>
+              <a href="filter.html" className="btn btn-primary">
+                Book Now
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="images/english.jpg"
+              className="card-img-top"
+              alt="english-img"
+            />
+            <div className="card-body">
+              <h4 className="card-title">ENGLISH</h4>
+              <p className="card-text">$12/hour</p>
+              <a href="filter.html" className="btn btn-primary">
+                Book Now
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card h-100">
+            <img
+              src="images/science.jpg"
+              className="card-img-top"
+              alt="science-img"
+            />
+            <div className="card-body">
+              <h4 className="card-title">SCIENCE</h4>
+              <p className="card-text">$12/hour</p>
+              <a href="filter.html" className="btn btn-primary">
+                Book Now
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </service>
+  );
+};
+
+export default Services;
