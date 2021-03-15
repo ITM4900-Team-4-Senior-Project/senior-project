f9be40b0ae71030905854c11e661d5480cb36005
hani0017 <77477500+hani0017@users.noreply.github.com>
2021-03-14 21:26:58 -0500

    Create signup.js

---
+++ src/pages/signup.js
@@ -0,0 +1,15 @@
+// Import necessary packages
+import { Helmet } from 'react-helmet'
+
+const Signup = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Happy Homework Helpers | Signup</title>
+      </Helmet>
+      <h1>Sign Up For Happy Homework Helpers</h1>
+    </div>
+  )
+}
+
+export default Signup
